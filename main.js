099e3c5703cc26c3f790396555c7a65bb49c6a66
lucaspopov@protonmail.com <oLucasPopov>
2022-04-19 19:41:49 -0300

    lesson 5 + excercise

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: [
+                {id: 157, description: 'Small'},
+                {id: 158, description: 'Medium'},
+                {id: 159, description: 'Large'},
+            ]
         }
     }
 })
