4f05cc1f3013132b4b32513ee2fe372442971266
lucaspopov@protonmail.com <oLucasPopov>
2022-04-21 17:17:06 -0300

    lesson 10 + excercise

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id){
+            this.cart.push(id)
+        },
+        removeFromCart(id){
+            const arrayIndex = this.cart.indexOf(id)
+            if(arrayIndex > -1) this.cart.splice(arrayIndex, 1)
+        },
+    }
 })
